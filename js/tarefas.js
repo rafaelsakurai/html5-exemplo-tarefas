ee71ed578723b84049d950d6aed94a9c26b26152
rafaelsakurai <rafasakurai@gmail.com>
2012-07-15 18:42:51 -0300

    Exemplo com HTML5

---
+++ js/tarefas.js
@@ -0,0 +1,33 @@
+window.onload = init;
+
+function init() {
+  var button = document.getElementById("botaoAdicionar");
+  button.onclick = criarTarefa;
+
+  for(var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    if(key.substring(0, 6) == "tarefa") {
+      var value = localStorage.getItem(key);
+      adicionarTarefaNoDOM(value);
+    }
+  }
+}
+
+function adicionarTarefaNoDOM(value) {
+  var tarefas = document.getElementById("tarefas");
+  var tarefa = document.createElement("li");
+  //sticky.innerHTML = value;
+  var span = document.createElement("span");
+  span.setAttribute("class", "tarefa");
+  span.innerHTML = value;
+  tarefa.appendChild(span);
+  tarefas.appendChild(tarefa);
+}
+
+function criarTarefa() {
+  var valor = document.getElementById("descricao").value;
+  var chave = "tarefa_" + localStorage.length;
+  localStorage.setItem(chave, valor);
+  adicionarTarefaNoDOM(valor);
+  document.getElementById("descricao").value = "";
+}
